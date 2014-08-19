caa4a8b108b23ea5ded353c4b64ff692fdebf5f3
magicbacon <sac.dongyuan@gmail.com>
2014-08-18 23:35:30 -0400

    Add json builder code.

--- JsonP/src/main/java/com/yuandong/jsonp/App.java
+++ JsonP/src/main/java/com/yuandong/jsonp/App.java
@@ -1,13 +1,29 @@
 package com.yuandong.jsonp;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		JsonObject model = Json.createObjectBuilder()
+				.add("firstName", "Duke")
+				.add("lastName", "Java")
+				.add("age", 18)
+				.add("streetAddress", "100 Internet Dr")
+				.add("city", "JavaTown")
+				.add("state", "JA")
+				.add("postalCode", "12345")
+				.add("phoneNumber", Json.createArrayBuilder()
+						.add(Json.createObjectBuilder()
+								.add("type", "mobile")
+								.add("number", "111-111-1111"))
+						.add(Json.createObjectBuilder()
+								.add("type", "home")
+								.add("number", "222-222-2222")))
+				.build();
+	}
 }
